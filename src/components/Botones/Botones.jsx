87b3ceaa9fa3f4b9aae552e9965dd4c0f4cfa502
Sabando Gonzalez <lolia_15@hotmail.com>
2018-09-25 21:02:59 -0400

    botones funcionales

--- src/components/Botones/Botones.jsx
+++ src/components/Botones/Botones.jsx
@@ -11,18 +11,18 @@ class Botones extends Component {
       <div>
         <div className="col-12 col-xs-12 col-md-12 col-lg-12 col-xl-12">
           <div className="buttons">
-            <a className="btn btn-primary paisajes" name="Paisaje" href="#"  role="button" onClick={this.actualizarBusqueda} >Paisajes</a>
+            <a className="btn btn-primary paisajes" name="Paisaje" href="#"  role="button" onClick={this.actualizarBusqueda}>Paisajes</a>
             <a className="btn btn-primary comida" name="Comida" href="#" role="button" onClick={this.actualizarBusqueda}>Comida</a>
-            <a className="btn btn-primary bodas" href="#" role="button">Bodas</a>
-            <a className="btn btn-primary idiomas" href="#" role="button">Idiomas</a>
-            <a className="btn btn-primary reposteria" href="#" role="button">Repostería</a>
-            <a className="btn btn-primary decoracion" href="#" role="button">Decoración</a>
-            <a className="btn btn-primary tecnologia" href="#" role="button">Tecnología</a>
-            <a className="btn btn-primary moda" href="#" role="button">Moda</a>
-            <a className="btn btn-primary populares" href="#" role="button">Populares</a>
-            <a className="btn btn-primary regalos" href="#" role="button">Regalos</a>
-            <a className="btn btn-primary arquitectura" href="#" role="button">Arquitectura</a>
-            <a className="btn btn-primary logos" href="#" role="button">Logos</a>
+            <a className="btn btn-primary bodas" name="Bodas" href="#" role="button" onClick={this.actualizarBusqueda}>Bodas</a>
+            <a className="btn btn-primary idiomas" name="Idiomas" href="#" role="button" onClick={this.actualizarBusqueda}>Idiomas</a>
+            <a className="btn btn-primary reposteria" name="Reposteria" href="#" role="button" onClick={this.actualizarBusqueda}>Repostería</a>
+            <a className="btn btn-primary decoracion" name="Decoracion" href="#" role="button" onClick={this.actualizarBusqueda}>Decoración</a>
+            <a className="btn btn-primary tecnologia" name="Tecnologia" href="#" role="button" onClick={this.actualizarBusqueda}>Tecnología</a>
+            <a className="btn btn-primary moda" name="Moda" href="#" role="button" onClick={this.actualizarBusqueda}>Moda</a>
+            <a className="btn btn-primary populares" name="Populares" href="#" role="button" onClick={this.actualizarBusqueda}>Populares</a>
+            <a className="btn btn-primary regalos" name="Regalos" href="#" role="button" onClick={this.actualizarBusqueda}>Regalos</a>
+            <a className="btn btn-primary arquitectura" name="Arquitectura" href="#" role="button" onClick={this.actualizarBusqueda}>Arquitectura</a>
+            <a className="btn btn-primary logos" name="Logos" href="#" role="button" onClick={this.actualizarBusqueda}>Logos</a>
           </div>
         </div>
       </div>
