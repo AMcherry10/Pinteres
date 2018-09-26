9e6de9689a7955d7ea6820ecae1938c79e7774ed
Sabando Gonzalez <lolia_15@hotmail.com>
2018-09-25 21:15:07 -0400

    agregue didmount

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
   state = {
     resultadoBusqueda: [],
   }
+  
+  componentDidMount(){
+    this.actualizarBusqueda()
+  }
 
-  actualizarBusqueda(nuevaBusqueda){
-    nuevaBusqueda.replace(" ","+")
+
+  actualizarBusqueda(nuevaBusqueda = ""){ /* esto es para q si o si nuevabusqueda tenga algo y se pueda ocupar replace, sino, lo toma como nulo y tira error */
+    nuevaBusqueda.replace(" ","+") /* esto es para q cuando se busque algo con espacio en url se ponga + */
     fetch(`https://pixabay.com/api/?key=9828833-9a15a3dc938016b93df55afe1&q=${nuevaBusqueda}&image_type=photo&lang=es`)
       .then(response => response.json()) 
       .then(datos =>{ 
@@ -19,7 +24,6 @@ class App extends Component {
   }
 
   render() {
-    console.log("Render!")
     return (
       <div > {/* barra llama a funcion y regresa con el nuevo valor q se obtuvo del input del componente barra */}
        <Barra actualizarBusqueda={this.actualizarBusqueda.bind(this)} /> {/* actualizarBusqueda1 es el props y el segundo es el valor de ese props q es una funcion */}
