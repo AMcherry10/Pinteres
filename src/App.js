c1344bd2acb3a0ba288a6f81fcf456d7f353384e
Sabando Gonzalez <lolia_15@hotmail.com>
2018-09-26 22:55:36 -0400

    scroll

--- src/App.js
+++ src/App.js
@@ -5,30 +5,67 @@ import './App.css';
 import Imagenes from './components/Imagenes/Imagenes';
 
 class App extends Component {
-  state = {
-    resultadoBusqueda: [],
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      busqueda: "",
+      resultadoBusqueda: [],
+      page: 1,
+      cargando: true,
+    }
+
+    window.onscroll = () => {
+      if (this.state.cargando === true) {
+        return
+      }
+      if (
+        window.innerHeight + document.documentElement.scrollTop
+        === document.documentElement.offsetHeight
+      ) {
+        this.cargarMasImagenes();
+      }
+    };
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.actualizarBusqueda()
   }
 
+  cargarMasImagenes = () => {
+    let pageNew = this.state.page + 1
+    let imagenesAntiguas = this.state.resultadoBusqueda
+    fetch(`https://pixabay.com/api/?key=9828833-9a15a3dc938016b93df55afe1&q=${this.state.busqueda}&image_type=photo&lang=es&page=${pageNew}`)
+      .then(response => response.json())
+      .then(datos => {
+        this.setState({
+          resultadoBusqueda: imagenesAntiguas.concat(datos.hits),
+          page: pageNew,
+          cargando: false,
+        })
+      })
+  }
 
-  actualizarBusqueda(nuevaBusqueda = ""){ /* esto es para q si o si nuevabusqueda tenga algo y se pueda ocupar replace, sino, lo toma como nulo y tira error */
-    nuevaBusqueda.replace(" ","+") /* esto es para q cuando se busque algo con espacio en url se ponga + */
+  actualizarBusqueda(nuevaBusqueda = "") { /* esto es para q si o si nuevabusqueda tenga algo y se pueda ocupar replace, sino, lo toma como nulo y tira error */
+    nuevaBusqueda.replace(" ", "+") /* esto es para q cuando se busque algo con espacio en url se ponga + */
     fetch(`https://pixabay.com/api/?key=9828833-9a15a3dc938016b93df55afe1&q=${nuevaBusqueda}&image_type=photo&lang=es`)
-      .then(response => response.json()) 
-      .then(datos =>{ 
-        this.setState({resultadoBusqueda: datos.hits})
+      .then(response => response.json())
+      .then(datos => {
+        this.setState({
+          resultadoBusqueda: datos.hits,
+          busqueda: nuevaBusqueda,
+          page: 1,
+          cargando: false,
+        })
       })
   }
 
   render() {
     return (
       <div > {/* barra llama a funcion y regresa con el nuevo valor q se obtuvo del input del componente barra */}
-       <Barra actualizarBusqueda={this.actualizarBusqueda.bind(this)} /> {/* actualizarBusqueda1 es el props y el segundo es el valor de ese props q es una funcion */}
-       <Botones actualizarBusqueda={this.actualizarBusqueda.bind(this)} />
-       <Imagenes dataImagenes={this.state.resultadoBusqueda} />
+        <Barra actualizarBusqueda={this.actualizarBusqueda.bind(this)} /> {/* actualizarBusqueda1 es el props y el segundo es el valor de ese props q es una funcion */}
+        <Botones actualizarBusqueda={this.actualizarBusqueda.bind(this)} />
+        <Imagenes dataImagenes={this.state.resultadoBusqueda} />
       </div>
     );
   }
